64433f8660cfd9dae22c7fe9193706b4c01b3de5
pablo-data <pablo.acu.ramirez@gmail.com>
2021-04-01 16:01:37 -0300

    feat: add function store

--- components/message/network.js
+++ components/message/network.js
@@ -5,7 +5,8 @@ const controller = require('./controller');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    controller.getMessages()
+    const filterMessages = req.query.user || null;
+    controller.getMessages(filterMessages)
     .then((messageList) => {
         response.success(req, res, messageList, 200);
     })
@@ -24,6 +25,26 @@ router.post('/', function(req, res){
         response.error(req, res, 'Informacion invalida', 400, 'Error en el controlador');
    });
    
+});
+
+router.patch('/:id', function(req, res){
+    controller.updateMessage(req.params.id, req.body.message)
+    .then((data) => {
+        response.success(req, res, data, 200);
+    })
+    .catch(e => {
+        response.error(req, res, 'Error Interno', 500, e);
+    });
+})
+
+router.delete('/:id', function(req, res) {
+    controller.deleteMessage(req.params.id)
+    .then(() => {
+        response.success(req, res, `Usuario ${req.params.id} eliminado`, 200);
+    })
+    .catch(e => {
+        response.error(req, res, 'Error interno', 500, e);
+    })
 });
 
 module.exports = router;
--- components/message/store.js
+++ components/message/store.js
@@ -1,17 +1,50 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model');
+
+const uri = 'ingresar';
+
+db.Promise = global.Promise;
+
+db.connect(uri, {useNewUrlParser:true,
+useUnifiedTopology: true })
+.then(() => console.log('[db] Conectada con éxito'))
+.catch(err => console.error('[db]', err));
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessages() {
-    return list;
+async function getMessages(filterUser) {
+    let filter = {};
+    if(filterUser !== null){
+        filter = { user: filterUser}
+    }
+   const messages = await Model.find(filter);
+   return messages;
+}
+
+async function updateText(id, message){
+    const foundMessage = await Model.findOne({
+        _id: id
+    });
+
+    foundMessage.message = message; 
+    const newMesssage = await foundMessage.save();
+    return newMesssage;
+}
+
+async function removeMessage(id) {
+    const exist = await Model.deleteOne({
+        _id: id
+    });
+
+    return exist;
 }
 
 module.exports = {
     add: addMessage,
     list: getMessages,
-    //get
-    //update
-    //delete
+    updateText: updateText,
+    remove: removeMessage,
 }
